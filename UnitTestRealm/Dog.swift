705ffae27fc8b64e7e88da704dad062c2c4afd18
Hoang Tran <hoangtx.master@gmail.com>
2016-10-11 08:40:43 +0700

    test to-one relationship

---
+++ UnitTestRealm/Dog.swift
@@ -0,0 +1,15 @@
+//
+//  Dog.swift
+//  UnitTestRealm
+//
+//  Created by Hoang Tran on 10/11/16.
+//  Copyright © 2016 Hoang Tran. All rights reserved.
+//
+
+import Realm
+import RealmSwift
+
+class Dog: Object {
+  dynamic var name = ""
+  dynamic var owner: Person?
+}
--- UnitTestRealmTests/PersonSpec.swift
+++ UnitTestRealmTests/PersonSpec.swift
@@ -8,20 +8,41 @@
 
 import Quick
 import Nimble
+import RealmSwift
 @testable import UnitTestRealm
 
 class PersonSpec: BaseSpec {
   override func spec() {
     super.spec()
 
-    describe("initialize with name and age") {
-      let personName = "name A"
-      let personAge = 18
+    let personName = "name A"
+    let personAge = 18
 
+    describe("initialize with name and age") {
       it("initializes correctly") {
         let person = Person(name: personName, age: personAge)
         expect(person.name) == personName
         expect(person.age) == personAge
+      }
+    }
+
+    describe("relationships") {
+      let dogName = "dog name A"
+
+      describe("to-one relationship") {
+        it("saves the object and its relationship to database correctly") {
+          let person = Person(name: personName, age: personAge)
+          let dog = Dog(value: ["name": dogName])
+          dog.owner = person
+          let realm = try! Realm()
+          try! realm.write {
+            realm.add(dog)
+          }
+          let dogFromDatabase = realm.objects(Dog.self).first
+          expect(dogFromDatabase?.name) == dogName
+          expect(dogFromDatabase?.owner?.name) == person.name
+          expect(dogFromDatabase?.owner?.age) == person.age
+        }
       }
     }
   }
